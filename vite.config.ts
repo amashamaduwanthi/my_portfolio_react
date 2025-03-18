99d45fc51f9d1bcfb3e0f73d445ecd8afc9d5fea
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-03-18 11:19:46 +0530

    npm build new

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/my_portfolio_react/',  // Set base path for GitHub Pages deployment
+  plugins: [react()],  // Ensure React plugin is included
+});
